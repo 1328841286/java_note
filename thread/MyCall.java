eab38a7ecd6eed7892bda5d9d8acd50178b7628e
xinwang <1328841286@qq.com>
2025-08-04 23:03:17 +0800

    【多线程】多线程三种实现方式

--- thread/MyCall.java
+++ thread/MyCall.java
@@ -14,6 +14,7 @@ import java.util.concurrent.Callable;
 public class MyCall implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
+
         int sum = 0;
         for (int i = 0; i < 100; i++) {
             sum += i;
